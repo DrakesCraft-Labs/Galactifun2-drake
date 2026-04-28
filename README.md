c0e1229f621d711143568cffd40957ab530d67e8
Pablo Elías Avendaño Miranda <pablo.elias.miranda.292003@gmail.com>
2026-04-28 11:14:49 -0400

    feat: bootstrap standalone addon repository

---
+++ README.md
@@ -0,0 +1,24 @@
+# Galactifun2-drake
+
+## Resumen Drake
+Addon espacial de Slimefun con planetas, generacion de mundos y progresion avanzada.
+
+## Que anade a Slimefun
+- Extiende contenido de Slimefun con mecanicas y bloques propios del addon.
+- Incluye ajustes de compatibilidad para stack Drake 1.21.
+
+## Estado
+- Extraido desde drakes-slimefun-labs (rama 1.21-latin).
+- Objetivo: desarrollo aislado por addon y releases independientes.
+
+---
+
+# Galactifun2
+
+This is a rewrite of [Galactifun](https://github.com/Slimefun-Addon-Community/Galactifun) in Kotlin. Also planning to redo the rocket system.
+
+## Credits
+
+Big thanks to [minecraft-heads](https://minecraft-heads.com/) for providing the heads used in this plugin.
+
+[![](https://images.minecraft-heads.com/banners/minecraft-heads_fullbanner_468x60.png)](https://minecraft-heads.com/)
---
+++ plugin/src/main/kotlin/io/github/addoncommunity/galactifun/util/items/ItemBuilder.kt
@@ -0,0 +1,136 @@
+package io.github.addoncommunity.galactifun.util.items
+
+import io.github.addoncommunity.galactifun.Galactifun2
+import io.github.addoncommunity.galactifun.util.bukkit.legacyDefaultColor
+import io.github.addoncommunity.galactifun.util.bukkit.miniMessageToLegacy
+import io.github.addoncommunity.galactifun.util.general.RequiredProperty
+import com.github.drakescraft_labs.slimefun4.api.items.ItemGroup
+import com.github.drakescraft_labs.slimefun4.api.items.SlimefunItem
+import com.github.drakescraft_labs.slimefun4.api.items.SlimefunItemStack
+import com.github.drakescraft_labs.slimefun4.api.recipes.RecipeType
+import com.github.drakescraft_labs.slimefun4.utils.SlimefunUtils
+import io.github.addoncommunity.galactifun.units.Force
+import io.github.addoncommunity.galactifun.units.Volume
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import java.lang.reflect.Constructor
+import kotlin.time.Duration
+
+class ItemBuilder {
+
+    var name: String by RequiredProperty()
+    var material: MaterialType by RequiredProperty()
+    var id: String by RequiredProperty(setter = { "GF2_" + it.uppercase() })
+
+    var category: ItemGroup by RequiredProperty()
+    var recipeType: RecipeType by RequiredProperty()
+    var recipe: Array<out ItemStack?> by RequiredProperty()
+
+    private val lore = mutableListOf<String>()
+
+    operator fun String.unaryPlus() {
+        lore += this.miniMessageToLegacy()
+    }
+
+    fun build(clazz: Class<out SlimefunItem>, vararg otherArgs: Any?): SlimefunItemStack {
+        val sfi = SlimefunItemStack(
+            id,
+            material.convert(),
+            name.miniMessageToLegacy().legacyDefaultColor('f'),
+            *lore.map { it.legacyDefaultColor('7') }.toTypedArray()
+        )
+        val ctor = findStandardSlimefunConstructor(clazz, otherArgs.size)
+            ?: error("No constructor (ItemGroup, SlimefunItemStack, RecipeType, ItemStack[], …) for ${clazz.name} with ${otherArgs.size} trailing args")
+        ctor.isAccessible = true
+        try {
+            val args = buildConstructorArgs(ctor, category, sfi, recipeType, recipe, otherArgs)
+            val item = ctor.newInstance(*args) as SlimefunItem
+            item.register(Galactifun2)
+        } catch (e: ReflectiveOperationException) {
+            throw RuntimeException("Failed to construct ${clazz.name}", e)
+        }
+        return sfi
+    }
+
+    /**
+     * Los `value class` de Kotlin (p. ej. [Volume], [Force], [Duration]) se exponen en el constructor JVM
+     * como primitivos; hay que desempaquetar antes de [Constructor.newInstance].
+     */
+    private fun buildConstructorArgs(
+        ctor: Constructor<out SlimefunItem>,
+        category: ItemGroup,
+        sfi: SlimefunItemStack,
+        recipeType: RecipeType,
+        recipe: Array<out ItemStack?>,
+        otherArgs: Array<out Any?>
+    ): Array<Any?> {
+        val p = ctor.parameterTypes
+        val head: Array<Any?> = arrayOf(category, sfi, recipeType, recipe)
+        val tail = Array(otherArgs.size) { i ->
+            coerceJvmValue(p[4 + i], otherArgs[i])
+        }
+        return arrayOf(*head, *tail)
+    }
+
+    private fun coerceJvmValue(param: Class<*>, arg: Any?): Any? {
+        if (arg == null) return null
+        return when {
+            param == java.lang.Double.TYPE && arg is Volume -> arg.cubicMeters
+            param == java.lang.Double.TYPE && arg is Force -> arg.newtons
+            param == java.lang.Long.TYPE && arg is Duration -> arg.inWholeNanoseconds
+            else -> arg
+        }
+    }
+
+    private fun findStandardSlimefunConstructor(
+        clazz: Class<out SlimefunItem>,
+        trailingArgCount: Int
+    ): Constructor<out SlimefunItem>? {
+        val expected = 4 + trailingArgCount
+        @Suppress("UNCHECKED_CAST")
+        return clazz.declaredConstructors.asSequence()
+            .filter { it.parameterCount == expected && !it.isSynthetic }
+            .map { it as Constructor<out SlimefunItem> }
+            .firstOrNull { ctor ->
+                val p = ctor.parameterTypes
+                ItemGroup::class.java.isAssignableFrom(p[0]) &&
+                    SlimefunItemStack::class.java.isAssignableFrom(p[1]) &&
+                    RecipeType::class.java.isAssignableFrom(p[2]) &&
+                    p[3].isArray &&
+                    org.bukkit.inventory.ItemStack::class.java.isAssignableFrom(p[3].componentType)
+            }
+    }
+}
+
+sealed interface MaterialType {
+
+    fun convert(): org.bukkit.inventory.ItemStack
+
+    class Material(private val material: org.bukkit.Material) : MaterialType {
+        override fun convert() = ItemStack(material)
+    }
+
+    class ItemStack(private val itemStack: org.bukkit.inventory.ItemStack) : MaterialType {
+        override fun convert() = itemStack
+    }
+
+    class Head(private val texture: String) : MaterialType {
+        override fun convert() = SlimefunUtils.getCustomHead(texture)
+    }
+}
+
+val Material.materialType get() = MaterialType.Material(this)
+val ItemStack.materialType get() = MaterialType.ItemStack(this)
+
+inline fun <reified I : SlimefunItem> buildSlimefunItem(
+    vararg otherArgs: Any?,
+    builder: ItemBuilder.() -> Unit
+): SlimefunItemStack {
+    return ItemBuilder().apply(builder).build(I::class.java, *otherArgs)
+}
+
+inline fun buildSlimefunItem(
+    builder: ItemBuilder.() -> Unit
+): SlimefunItemStack {
+    return ItemBuilder().apply(builder).build(SlimefunItem::class.java)
+}
